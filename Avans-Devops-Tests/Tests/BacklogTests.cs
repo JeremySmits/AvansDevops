d33537ae4a59519ab34a98c2a917e0cfaccaa2c9
Jeremy <jeremy>
2022-03-23 16:31:21 +0100

    Old folder structure

--- Avans-Devops-Tests/Tests/BacklogTests.cs
+++ Avans-Devops-Tests/Tests/BacklogTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Avans_Devops.Tests
---
+++ Avans-Devops/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avans_Devops_Tests
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD!");
+        }
+    }
+}
