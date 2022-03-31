b357acbe0998c9b2e9b0ee24cebd13fe1a188edb
Sanne Huismans <scdhuism@avans.nl>
2022-03-31 13:43:59 +0200

    Made pipeline observable + tests

---
+++ Avans-Devops/Pipeline/TestPipeline.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using Avans_Devops.Observe;
+
+namespace Avans_Devops.Pipeline
+{
+	public class TestPipeline : IPipelineFactory, IObservable
+    {
+        public int PipelineId { get; set; }
+        public List<string> Sources { get; set; }
+        public List<string> Packages { get; set; }
+        public List<string> Builds { get; set; }
+        public List<string> Tests { get; set; }
+        public List<string> Analyses { get; set; }
+        public List<string> Deploys { get; set; }
+        public List<string> Utilities { get; set; }
+
+    }
+}
