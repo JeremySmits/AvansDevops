d96dac952a24c60ef416801a7d903ded9f175a99
Jeremy <jeremy>
2022-06-22 11:11:45 +0200

    Fixed errors

--- Avans-Devops/Pipeline/DeploymentPipeline.cs
+++ Avans-Devops/Pipeline/DeploymentPipeline.cs
@@ -4,20 +4,9 @@ using Avans_Devops.Observe;
 
 namespace Avans_Devops.Pipeline
 {
-	public class DeploymentPipeline : IPipeline, IObservable
+	public class DeploymentPipeline : Pipeline, IObservable
 	{
-		public string Title { get; set; }
-        public int PipelineId { get; set; }
-        public List<string> Sources { get; set; }
-        public List<string> Packages { get; set; }
-        public List<string> Builds { get; set; }
-        public List<string> Tests { get; set; }
-        public List<string> Analyses { get; set; }
-        public List<string> Deploys { get; set; }
-        public List<string> Utilities { get; set; }
-		public List<Observer> Observers { get; set;} = new List<Observer>();
-
-		public bool canRun()
+		public override bool canRun()
 		{
 			// A deployment pipeline may only run if it has deployments
 			return Deploys != null && Deploys.Count != 0;
--- Avans-Devops/Pipeline/TestPipeline.cs
+++ Avans-Devops/Pipeline/TestPipeline.cs
@@ -4,20 +4,9 @@ using Avans_Devops.Observe;
 
 namespace Avans_Devops.Pipeline
 {
-	public class TestPipeline : IPipeline, IObservable
+	public class TestPipeline : Pipeline, IObservable
 	{
-		public string Title { get; set; }
-        public int PipelineId { get; set; }
-        public List<string> Sources { get; set; }
-        public List<string> Packages { get; set; }
-        public List<string> Builds { get; set; }
-        public List<string> Tests { get; set; }
-        public List<string> Analyses { get; set; }
-        public List<string> Deploys { get; set; }
-        public List<string> Utilities { get; set; }
-		public List<Observer> Observers { get; set;} = new List<Observer>();
-
-		public bool canRun()
+		public override bool canRun()
 		{
 			// A testing pipeline may only run if it has tests, analyses (test results) and no deployments.
 			return hasTests() && hasAnalyses() && !hasDeployment();
