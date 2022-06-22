1542bb8e752d82639f8cdf932c6c49ad88cb6e3c
Jeremy <jeremy>
2022-06-22 11:07:33 +0200

    Removed dupilcations

--- Avans-Devops/Pipeline/GenericPipeline.cs
+++ Avans-Devops/Pipeline/GenericPipeline.cs
@@ -4,20 +4,9 @@ using Avans_Devops.Observe;
 
 namespace Avans_Devops.Pipeline
 {
-	public class GenericPipeline : IPipeline, IObservable
+	public class GenericPipeline : Pipeline, IObservable
 	{
-		public string Title { get; set; }
-        public int PipelineId { get; set; }
-        public List<string> Sources { get; set; }
-        public List<string> Packages { get; set; }
-        public List<string> Builds { get; set; }
-        public List<string> Tests { get; set; }
-        public List<string> Analyses { get; set; }
-        public List<string> Deploys { get; set; }
-        public List<string> Utilities { get; set; }
-		public List<Observer> Observers { get; set;} = new List<Observer>();
-
-		public bool canRun()
+		public override bool canRun()
 		{
 			// A generic pipeline may always run, even if it has no steps.
 			return true;
